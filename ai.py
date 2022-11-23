18e16eea88661c84697d6f0fe646b0de3ac3e3f2
Danguilhen <thomas.danguilhen@estaca.eu>
2022-11-23 16:41:24 +0100

    fix training

--- ai.py
+++ ai.py
@@ -1,4 +1,4 @@
-#from ipoly import load
+# from ipoly import load
 import skimage
 import numpy as np
 import skimage.morphology as morph
@@ -6,13 +6,17 @@ from skimage.segmentation import clear_border
 from skimage import filters
 from PIL.Image import fromarray as afficher
 import keras
-from keras.layers import Dense, Conv2D,MaxPooling2D
+from keras.layers import Dense, Conv2D, MaxPooling2D
 import pandas as pd
 import cv2
 from keras.preprocessing.image import ImageDataGenerator
 
+IMAGE_SIZE = 500
 
-def croper(image, margin=3):
+
+def croper(image: np.array, margin: int = 3):
+    if len(np.unique(image)) == 1:
+        raise Exception("The image is composed of a single color.")
     if len(image.shape) == 3:
         image_sum = image.sum(axis=2) % 765
     else:
@@ -25,11 +29,14 @@ def croper(image, margin=3):
         max(0, top_left[1] - margin) : bottom_right[1] + 1 + margin,
     ]
 
-def prepare_image(image):
+
+def prepare_image(image: np.array):
     gim = skimage.color.rgb2gray(image)
     threshold = filters.threshold_otsu(gim)
     binary_mask = gim < threshold
-    binary_mask = morph.remove_small_objects(binary_mask, 50000)
+    binary_mask = morph.remove_small_objects(
+        binary_mask, 0.07 * binary_mask.shape[0] * binary_mask.shape[1]
+    )
     binary_mask_cleared = clear_border(
         skimage.morphology.remove_small_holes(binary_mask, 300)
     )
@@ -38,34 +45,24 @@ def prepare_image(image):
     labeled_image, _ = skimage.measure.label(binary_mask, return_num=True)
     image[labeled_image == 0] = 255
     img = croper(image)
-    scale_percent = min(500 * 100 / img.shape[0], 500 * 100 / img.shape[1])
+    scale_percent = min(
+        IMAGE_SIZE * 100 / img.shape[0], IMAGE_SIZE * 100 / img.shape[1]
+    )
     width = int(round(img.shape[1] * scale_percent / 100))
     height = int(round(img.shape[0] * scale_percent / 100))
     dim = (width, height)
     resized = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
-    if resized.shape[1] != 500:
-        white = np.full((500, 500 - resized.shape[1], 3), 255, dtype=np.uint8)
+    if resized.shape[1] != IMAGE_SIZE:
+        white = np.full(
+            (IMAGE_SIZE, IMAGE_SIZE - resized.shape[1], 3), 255, dtype=np.uint8
+        )
         result = np.concatenate((resized, white), axis=1)
     else:
-        white = np.full((500 - resized.shape[0], 500, 3), 255, dtype=np.uint8)
+        white = np.full(
+            (IMAGE_SIZE - resized.shape[0], IMAGE_SIZE, 3), 255, dtype=np.uint8
+        )
         result = np.concatenate((resized, white), axis=0)
     return result
-
-
-def prepare_dataset():
-    df = load("labels.csv")
-    df = pd.get_dummies(
-        df, columns=["bord", "phyllotaxie", "typeFeuille", "ligneux"], drop_first=True
-    )
-    X_train = list()
-    Y_train = list()
-    for row in df.iterrows():
-        print("dataset/" + row[1].repo)
-        i = load("dataset/" + row[1].repo)
-        result = prepare_image(i)
-        Y_train.append(list(row[1][3:7]))
-        X_train.append(result)
-    return X_train, Y_train
 
 
 def create_model():
@@ -76,11 +73,11 @@ def create_model():
             kernel_size=3,
             padding="same",
             activation="relu",
-            input_shape=(500, 500, 3),
+            input_shape=(IMAGE_SIZE, IMAGE_SIZE, 3),
         )
     )
     model.add(Conv2D(64, kernel_size=3, padding="same", activation="relu"))
-    model.add(MaxPooling2D(pool_size=(3, 3)))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(keras.layers.Flatten())
     model.add(Dense(128, activation="relu"))
     model.add(Dense(4, activation="sigmoid"))
@@ -90,39 +87,40 @@ def create_model():
 
 def preprocess_extract_patch():
     def _preprocess_extract_patch(x):
-        img = prepare_image(x)
+        img = prepare_image(x.astype(np.uint8))
         return img
+
     return _preprocess_extract_patch
 
 
-
-def train_model( model):
-    df=pd.read_csv("labels.csv")
+def train_model(model):
+    df = pd.read_csv("labels.csv")
 
     df = pd.get_dummies(
         df, columns=["bord", "phyllotaxie", "typeFeuille", "ligneux"], drop_first=True
     )
     preprocessing_function = preprocess_extract_patch()
 
-    BATCH_SIZE=5
+    BATCH_SIZE = 5
 
-    train_datagen_aug = ImageDataGenerator(preprocessing_function=preprocessing_function,rotation_range=25)
-    columns=["bord_lisse"	,"phyllotaxie_oppose"	,"typeFeuille_simple"	,"ligneux_oui"]
-    train_generator=train_datagen_aug.flow_from_dataframe(
-    dataframe=df[:5],
-    directory="dataset",
-    x_col="repo",
-    y_col=columns,
-    batch_size=5,
-    seed=42,
-    shuffle=True,
-    class_mode="raw")
-
-
-
+    train_datagen_aug = ImageDataGenerator(
+        preprocessing_function=preprocessing_function, rotation_range=25
+    )
+    columns = ["bord_lisse", "phyllotaxie_oppose", "typeFeuille_simple", "ligneux_oui"]
+    train_generator = train_datagen_aug.flow_from_dataframe(
+        dataframe=df[:BATCH_SIZE],
+        directory="dataset",
+        x_col="repo",
+        y_col=columns,
+        batch_size=BATCH_SIZE,
+        seed=42,
+        shuffle=True,
+        class_mode="raw",
+        target_size=(IMAGE_SIZE, IMAGE_SIZE),
+    )
 
     callback = [
-        keras.callbacks.EarlyStopping(monitor="loss", patience=3),
+        # keras.callbacks.EarlyStopping(monitor="val_loss", patience=3),
         keras.callbacks.ModelCheckpoint(
             filepath="model_checkpoint",
             save_weights_only=True,
@@ -134,8 +132,8 @@ def train_model( model):
 
     H = model.fit(
         train_generator,
-        #validation_data=(X_test, Y_test),
-        #steps_per_epoch=len(X_train) // BATCH_SIZE,
+        # validation_data=(X_test, Y_test),
+        # steps_per_epoch=len(X_train) // BATCH_SIZE,
         epochs=1,
         verbose=1,
         callbacks=[callback],
